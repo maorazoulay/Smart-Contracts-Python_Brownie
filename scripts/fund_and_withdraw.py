c912a77a1c397eb568b04f53f65029997be2d04a
Maor <maorazoulay@gmail.com>
2022-03-28 21:18:00 -0400

    Added fund and withdraw scripts

---
+++ scripts/fund_and_withdraw.py
@@ -0,0 +1,10 @@
+from brownie import accounts, FundMe
+
+from scripts.helpful_scripts import get_account
+
+
+def fund():
+    fund_me = FundMe[-1]
+    account = get_account()
+    entrance_fee = fund_me.getEntranceFee()
+    print(entrance_fee)
